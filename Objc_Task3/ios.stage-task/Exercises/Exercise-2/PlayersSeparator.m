facffa8432cfcbeac8b2c61688156f061fd1e50b
Vladimir S <46943806+xk1b0r9x@users.noreply.github.com>
2021-05-24 23:49:42 +0300

    fix: two

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -1,21 +1,20 @@
 #import "PlayersSeparator.h"
-
 @implementation PlayersSeparator
 
-- (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-  NSMutableArray *ratesArray = [[NSMutableArray alloc] init];
-   for (int i = 0, count = (int)rateArray.count; i < count - 2; ++i) {
-      NSInteger iRates = rateArray[i].integerValue;
-       for (int j = i + 1; j < count - 1; ++j){
-         NSInteger jRates = rateArray[j].integerValue;
-         for (int k = j+1; k < count; ++k) {
-               NSInteger kRates = rateArray[k].integerValue;
-                if ((iRates < jRates && jRates < kRates) || (iRates > jRates && jRates > kRates)) {
-                  [ratesArray addObject:@[rateArray[i], rateArray[j], rateArray[k]]];
-             }
-         }
-     }
- }
-    return ratesArray.count;
+- (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)rateArray {
+NSMutableArray *ratesArray = [[NSMutableArray alloc] init];
+for (int i = 0, count = (int)rateArray.count; i < count - 2; ++i) {
+    NSInteger iTemp = rateArray[i].integerValue;
+    for (int j = i + 1; j < count - 1; ++j){
+        NSInteger jTemp = rateArray[j].integerValue;
+        for (int k = j + 1; k < count; ++k) {
+            NSInteger kTemp = rateArray[k].integerValue;
+            if ((iTemp < jTemp && jTemp < kTemp) || (iTemp > jTemp && jTemp > kTemp)) {
+                [ratesArray addObject:@[rateArray[i], rateArray[j], rateArray[k]]];
+            }
+        }
+    }
+}
+return ratesArray.count;
 }
 @end
