2d4353604a90e1f49243c4a69a1364a3176c9b20
Vladimir S <46943806+xk1b0r9x@users.noreply.github.com>
2021-05-24 22:24:38 +0300

    feat: add new ex2

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,19 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+  NSMutableArray *ratesArray = [[NSMutableArray alloc] init];
+   for (int i = 0, count = (int)rateArray.count; i < count - 2; ++i) {
+      NSInteger iNumber = rateArray[i].integerValue;
+       for (int j = i+1; j < count-1; ++j){
+           for (int k = j+1; k < count; ++k) {
+             NSInteger jNumber = rateArray[j].integerValue;
+               NSInteger kNumber = rateArray[k].integerValue;
+                if ((iNumber < jNumber && jNumber < kNumber) || (iNumber > jNumber && jNumber > kNumber)) {
+                  [ratesArray addObject:@[rateArray[i], rateArray[j], rateArray[k]]];
+             }
+         }
+     }
+ }
+    return ratesArray.count;
 }
-
 @end
