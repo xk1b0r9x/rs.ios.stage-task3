f44313e05f6dad4cd25bba25d893908db445836b
Vladimir S <46943806+xk1b0r9x@users.noreply.github.com>
2021-05-24 23:09:20 +0300

    fix: error this code

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -5,12 +5,12 @@
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
   NSMutableArray *ratesArray = [[NSMutableArray alloc] init];
    for (int i = 0, count = (int)rateArray.count; i < count - 2; ++i) {
-      NSInteger iNumber = rateArray[i].integerValue;
-       for (int j = i+1; j < count-1; ++j){
-           for (int k = j+1; k < count; ++k) {
-             NSInteger jNumber = rateArray[j].integerValue;
-               NSInteger kNumber = rateArray[k].integerValue;
-                if ((iNumber < jNumber && jNumber < kNumber) || (iNumber > jNumber && jNumber > kNumber)) {
+      NSInteger iRates = rateArray[i].integerValue;
+       for (int j = i + 1; j < count - 1; ++j){
+         NSInteger jRates = rateArray[j].integerValue;
+         for (int k = j+1; k < count; ++k) {
+               NSInteger kRates = rateArray[k].integerValue;
+                if ((iRates < jRates && jRates < kRates) || (iRates > jRates && jRates > kRates)) {
                   [ratesArray addObject:@[rateArray[i], rateArray[j], rateArray[k]]];
              }
          }
